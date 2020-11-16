2c1c8cad28deb7623e2e4a2e1a97428f65bcaa7a
Okechukwu Uneze <unezeo@gmail.com>
2020-11-16 14:27:15 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -25,7 +25,7 @@ end
 
 def end_game(card_total)
   # code #end_game here
-  puts "Sorry, you hit #{card_total}"
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
@@ -41,10 +41,13 @@ def hit?(card_total)
   # code hit? here
   prompt_user
   c = get_user_input
-  invalid_command
+
   if c == "h"
     v = deal_card
     card_total += v
+  elsif c != 'h' ||  c != 's'
+    invalid_command
+
   end
   card_total
 end
